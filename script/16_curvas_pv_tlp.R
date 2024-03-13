2cea6c2f90ca7b1c3e4765e9b501a08eb330633f
M. Abel Herrera <m.herreradiaz16@gmail.com>
2024-03-13 11:21:11 -0300

    13 mar. 2024

--- script/16_curvas_pv_tlp.R
+++ script/16_curvas_pv_tlp.R
@@ -1,14 +1,17 @@
 library(drc)
 library(xlsx)
 library(readxl)
+library(readr)
+library(dplyr)
+library(ggplot2)
 
-codigos <- excel_sheets('data/data_raw/pv/la_esperanza_pv.xlsx')[-8]
+codigos <- excel_sheets('data/data_raw/pv/la_esperanza_pv.xlsx')[-7]
 
-df <- data.frame(sitio = 'rio_claro',
+df <- data.frame(sitio = 'la_esperanza',
                  codigo = codigos,
                  TLP = NA)
 
-datos <- read.xlsx('data/data_raw/pv/rio_claro_pv_pr.xlsx', sheetName = codigos[n]) |>
+datos <- read_xlsx('data/data_raw/pv/la_esperanza_pv.xlsx', sheet = codigos[n]) |>
   mutate(potencial = -bar/10,
          agua_hoja = peso_cosechado-peso_seco,
          minus_y = -1/potencial)
@@ -16,10 +19,21 @@ datos <- read.xlsx('data/data_raw/pv/rio_claro_pv_pr.xlsx', sheetName = codigos[
 ggplot(datos[1:(nrow(datos)-x),], aes(x = agua_hoja, y = potencial)) +
   geom_point()
 
-datos$RWD <- 100-datos$agua_hoja/lm(agua_hoja ~ potencial, datos[1:(nrow(datos)-x),])$coefficients[1]*100
+datos$RWD <- 100-datos$agua_hoja/lm(agua_hoja ~ potencial, datos[1:(nrow(datos)-x+1),])$coefficients[1]*100
 
-df[n,3] <- -1/(lm(minus_y ~ RWD, datos[(x+1):nrow(datos),])$coefficients[1]+
-      lm(minus_y ~ RWD, datos[(x+1):nrow(datos),])$coefficients[2]*datos$RWD[x+1])
+int <- lm(minus_y ~ RWD, datos[(nrow(datos)-x+1):nrow(datos),])$coefficients[1]
+slp <- lm(minus_y ~ RWD, datos[(nrow(datos)-x+1):nrow(datos),])$coefficients[2]
+
+ggplot(datos,aes(x = RWD,y = minus_y)) +
+  geom_point() +
+  geom_abline(intercept = int, slope = slp, color = "red") +
+  labs(x = 'Deficit de agua relativo (%RWD)',
+       y = expression(paste("Inversa del potencial hídrico ", -1/psi, " (MPa"^-1,')'))) +
+  theme_light()
+  
+datos$Yo <- -1/(int+slp*datos$RWD)
+
+df[n,3] <- datos$Yo[nrow(datos)-x+1]
 
 write_rds(df,'data/data_postprocessed/tlp.rds')
 
