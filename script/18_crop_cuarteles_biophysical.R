80924aa8da5efb21dedbb25f588e378548431375
francisco <frzambra@gmail.com>
2024-07-02 17:35:38 -0400

    crop biophysical parameters

---
+++ script/18_crop_cuarteles_biophysical.R
@@ -0,0 +1,25 @@
+library(fs)
+library(terra)
+library(sf)
+library(tidyverse)
+library(glue)
+
+dir <- '/mnt/md0/raster_procesada/Sentinel2/Bio'
+
+files <- dir_ls(dir,recurse = TRUE,regexp = '(fcover|lai|fapar).*img$')
+
+var <- 'fcover'
+sitio <- 'rio_claro'
+tiles <- c('la_esperanza'='HBC','rio_claro'='HCB')
+tile <- tiles[sitio]
+files_sel <- files[str_detect(files,glue('T19{tile}.*{var}.img'))]
+
+names <- str_extract(files_sel,'T19.*_[0-9]{8}')
+pol <- st_read(glue('data/processed/espacial/sitios/{sitio}.gpkg'),'cuartel')
+
+lapply(seq_along(files_sel),\(i){
+  im <- rast(files_sel[i])
+  pol <- pol |> st_transform(crs(im))
+  im <- crop(im,pol)
+  writeRaster(im,glue('data/processed/espacial/raster/biophysical/{var}/{names[i]}_{sitio}_{var}.tif'))
+})
