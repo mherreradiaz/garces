ee0fbdba371929f2d3bc0c2ad6de37fed97a9282
frzambra <frzambra@gmail.com>
2024-08-05 16:03:11 -0400

    ajustar modelos

--- script/14_1_predecir_potencial_raster.R
+++ script/14_1_predecir_potencial_raster.R
@@ -2,17 +2,9 @@ library(tidyverse)
 library(fs)
 library(terra)
 
-files_vi <- dir_ls('data/processed/espacial/raster/vi_smooth')
-files_bio <- dir_ls('data/processed/espacial/raster/biopar_smooth')
+files <- dir_ls('data/processed/espacial/raster/predictores')
 
-dates_vi <- str_extract(files_vi,'[0-9]{4}_[0-9]{2}_[0-9]{2}') |> ymd()
-dates_bio <- str_extract(files_bio,'[0-9]{4}_[0-9]{2}_[0-9]{2}') |> ymd()
-
-dates_inter <- lubridate::intersect(dates_vi,dates_bio) |> as_date()
-
-files_vi[dates_vi %in% dates_inter] |> length()
-
-files_bio[dates_bio %in% dates_inter] |> length()
+dates <- str_extract(files,'[0-9]{4}_[0-9]{2}_[0-9]{2}') |> ymd()
 
 
 data_clima_1 <- read_rds('data/processed/clima_hora.rds') |> 
@@ -49,34 +41,35 @@ modelo <- read_rds('data/processed/modelos/xgboost_over.rds')
 
 out <- map(files,\(file){
   print(file)
-  s2 <- rast(file)
+  predictores <- rast(file)
   
-  if(!(is.na(values(s2)) |> any())){
-    ndwi <- s2['ndwi'] # Normalized Difference Water Index
-    ndmi <- s2['ndmi'] # Normalized Difference Moisture Index
-    msi <- s2['msi']# Moisture Stress Index
-    gci <- s2['gci'] # Green Coverage Index
-    ndvi <- s2['ndvi'] #NDVI
-    nbr <- s2['nbr']# Normalized Burn Ratio
-    b_i <- s2['b_i'] # Índice B11 Y B12
-    
-    sitio_filt <- str_extract(file,'(?<=smooth_)[a-zA-Z_]+(?=_\\d{4}_\\d{2}_\\d{2})')
-    fecha_filt <- gsub('_','-',str_extract(file,'\\d{4}_\\d{2}_\\d{2}'))
+  if(!(is.na(values(s2)) |> all())){
+    # ndwi <- s2['ndwi'] # Normalized Difference Water Index
+    # ndmi <- s2['ndmi'] # Normalized Difference Moisture Index
+    # msi <- s2['msi']# Moisture Stress Index
+    # gci <- s2['gci'] # Green Coverage Index
+    # ndvi <- s2['ndvi'] #NDVI
+    # nbr <- s2['nbr']# Normalized Burn Ratio
+    # b_i <- s2['b_i'] # Índice B11 Y B12
+    names(predictores) <- tolower(names(predictores))
+      
+    sitio_filt <- str_extract(file,'(?<=SMOOTH_)[a-zA-Z_]+(?=_\\d{4}\\d{2}\\d{2})')
+    fecha_filt <- gsub('_','-',str_extract(file,'\\d{4}\\d{2}\\d{2}'))
     data_clima_x <- data_clima |> 
       filter(sitio == sitio_filt,
-             fecha  == fecha_filt)
+             fecha  == ymd(fecha_filt))
     
     #crear raster para prediccion
     
-    rast_temp <- s2[[1]]
+    rast_temp <- predictores[[1]]
     values(rast_temp) <- data_clima_x$t_media
-    rast_rh <- s2[[1]]
+    rast_rh <- predictores[[1]]
     values(rast_rh) <- data_clima_x$rh_media
-    rast_vpd <- s2[[1]]
+    rast_vpd <- predictores[[1]]
     values(rast_vpd) <- data_clima_x$vpd_medio
-    rast_eto <- s2[[1]]
+    rast_eto <- predictores[[1]]
     values(rast_eto) <- data_clima_x$eto
-    rast_pp <- s2[[1]]
+    rast_pp <- predictores[[1]]
     values(rast_pp) <- data_clima_x$pp
     
     # "B01"           "B02"           "B03"          
@@ -84,11 +77,8 @@ out <- map(files,\(file){
     # [9] "B08"           "B09"           "B11"           "B12"     
     # [13] "B8A"           "ndwi"          "ndmi"          "msi"    
     # [17] "gci"           "t_media"       "rh_media"      "vpd_medio"    
-    
-    predictores <- c(ndwi,ndmi,msi,gci,ndvi,nbr,b_i,
-                     rast_temp,rast_rh,rast_vpd,rast_eto,rast_pp)
-    names(predictores) <- c('ndwi','ndmi','msi','gci','ndvi','nbr','b_i',
-                            't_media','rh_media','vpd_medio','eto','pp')    
+    predictores <- c(rast_temp,rast_rh,rast_vpd,rast_eto,rast_pp,predictores)
+    names(predictores)[1:5] <- c('t_media','rh_media','vpd_medio','eto','pp')    
     
     #funcion para aplicar el modelo en los rasters 
     fun<-function(...){
@@ -101,7 +91,7 @@ out <- map(files,\(file){
                      model =modelo,
                      fun=fun)
   } else {
-    potencial_raster <- s2[[1]]
+    potencial_raster <- predictores[[1]]
     values(potencial_raster) <- NA
   }
   
@@ -109,7 +99,7 @@ out <- map(files,\(file){
   potencial_raster
 })
 
-names(out) <- str_extract(names(out),'(?<=smooth_)[^\\.]+(?=\\.tif)')
+names(out) <- str_extract(names(out),'(?<=SMOOTH_)[^\\.]+(?=\\.tif)')
 
 lapply(names(out), function(name) {
   dir <- paste0('data/processed/espacial/raster/potencial_predict/potencial_xgb_', name, '.tif')
--- script/17_mapas_potencial.R
+++ script/17_mapas_potencial.R
@@ -24,18 +24,20 @@ cov_rc <- global(swp_rc,\(x) sd(x,na.rm  =TRUE)/mean(x,na.rm = TRUE)) |>
   as_tibble()
 
 dates_max_cov_rc <- cov_rc |> 
-  arrange(desc(cov)) |> 
-  slice_head(n = 6) |> 
+  #arrange(desc(cov)) |> 
+  slice_min(cov,n = 6) |> 
   pull(dates)
 
-ids <- match(dates_max_cov_rc,names(swp_rc))
+ids <- match(sort(dates_max_cov_rc),names(swp_rc))
 
 swp_rc_mxcov <- subset(swp_rc,ids)
 
-map1 <- tm_shape(-swp_rc_mxcov*0.1) +
-  tm_raster(title = 'SWP (MPa)',style = 'cont', palette = diverging_hcl(palette = 'Green-Brown',n=7) ) +
-  tm_facets(nrow = 2,,free.scales = TRUE) +
-  tm_layout(legend.outside = FALSE)
+map1 <- tm_shape(swp_rc_mxcov*0.1) +
+  tm_raster(title = expression(Psi[S]~~(MPa)),style = 'cont', palette = diverging_hcl(palette = 'Green-Brown',n=7) ) +
+  tm_facets(nrow = 2,free.scales = TRUE) +
+  tm_layout(legend.outside = TRUE,
+            panel.label.bg.color = 'white',
+            panel.labels = ymd(names(swp_rc_mxcov)))
 
 tmap_save(map1,'output/figs/mapa_potencial_max_cov_rio_claro.png')
 
@@ -53,17 +55,20 @@ cov_le <- global(swp_le,\(x) sd(x,na.rm  =TRUE)/mean(x,na.rm = TRUE)) |>
   as_tibble()
 
 dates_max_cov_le <- cov_le |> 
-  arrange(desc(cov)) |> 
-  slice_head(n = 5) |> 
+  slice_min(cov,n = 6) |> 
   pull(dates)
 
-ids <- match(dates_max_cov_le,names(swp_le))
+ids <- match(sort(dates_max_cov_le),names(swp_le))
 
 swp_le_mxcov <- subset(swp_le,ids)
 
-map2 <- tm_shape(-swp_le_mxcov*0.1) +
-  tm_raster(title = 'SWP (MPa)',style = 'cont', palette = diverging_hcl(palette = 'Green-Brown',n=7) ) +
+map2 <- tm_shape(swp_le_mxcov*0.1) +
+  tm_raster(title = expression(Psi[S]~~(MPa)),style = 'cont', palette = diverging_hcl(palette = 'Green-Brown',n=7) ) +
   tm_facets(nrow = 2,,free.scales = TRUE) +
-  tm_layout(legend.outside = FALSE,legend.position = c('right','top'))
+  tm_layout(legend.outside = TRUE,
+            legend.position = c('right','top'),
+            panel.label.bg.color = 'white',
+            panel.labels = ymd(names(swp_rc_mxcov)))
+            
 
 tmap_save(map2,'output/figs/mapa_potencial_max_cov_la_esperanza.png')
